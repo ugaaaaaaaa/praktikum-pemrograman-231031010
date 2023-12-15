bf648efbb9354115d0b5eb31d87cf8a037a9c6ae
ugaaaaaaaa <153788363+ugaaaaaaaa@users.noreply.github.com>
2023-12-15 19:47:18 +0800

    Add files via upload

---
+++ 231031010-Anugrah Dwi Ansarna-tugas-dictionary-biodata-9-A.py
@@ -0,0 +1,10 @@
+biodata = {
+    "nama" : "Anugrah Dwi Ansarna",
+    "TTL" : "Parepare, 1 Maret 2005",
+    "Hobi" : ["Olahraga", "Main Gitar", "Baca novel", "Menggambar"],
+    "Agama" : "Islam",
+    "Pekerjaan" : "Mahasiswa"   
+}
+
+for i in biodata:
+    print(biodata[i])
